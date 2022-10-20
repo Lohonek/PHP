e5a238bdcea33a135f7f1aaa2d9effd1810c54f2
Lohonek <65301589+Lohonek@users.noreply.github.com>
2022-10-20 12:32:53 +0200

    Add files via upload

--- tablice.php
+++ tablice.php
@@ -8,24 +8,51 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+
+  <form action="" method="post">
+    <input type="text" name="losowa" placeholder="losowa">
+    <input type="submit">
+  </form>
     <?php
 
-@$rows = 11;
-@$cols = 11;
+    @$losowa = $_POST['losowa'];
+    @$random = rand(0,100)."<br>";
+    echo($random);
 
-echo("Zad 1"."<table>");
+    echo("<table>");
 
-for ($r =1; $r < $rows; $r++){
-    echo'<tr>';
-    for ($c = 1; $c < $cols; $c++)
-          echo '<td>' .$c*$r.'</td>';
-   echo '</tr>';
-
-}
-echo("<br> <br>");
-echo"</table>";
+    for($i = 1; $i <= $losowa; $i++){
+       echo("<td>".$random."<?td>");
+    }
 
 
+
+
+
+
+
+
+  // @$rows = 11;
+  // @$cols = 11;
+
+  // echo("Zad 1"."<table>");
+
+  // for ($r =1; $r < $rows; $r++){
+  //   echo'<tr>';
+  //   for ($c = 1; $c < $cols; $c++)
+  //         echo '<td>' .$c*$r.'</td>';
+  //  echo '</tr>';
+
+  // }
+  // echo("<br> <br>");
+  // echo"</table>";
+
+//Pole tekstowe ile liczb i wyswietl  liczby z zakresu 0 100 w tabeli po 20 w wierszu
+// 1 liczba ostania, srodkowa (parzysta nie parzysta) 
+//Suma
+//Srednia
+// W nowej tabeli wyswietl parzyste po 5
+// W nowej tabeli wyswietl nieparzyste po 6
 ?>
 
 <style>
@@ -34,7 +61,6 @@ td,
 tr {
   border: 1px solid black;
   border-radius: 15px;
-  padding: 20px;
   background-color: green;
   color: white;
   font-size: 20px;
